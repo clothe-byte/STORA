33f323898aac49177def32db1e174eeff63f12df
clothe-byte <ashahzain123@gmail.com>
2026-07-04 18:39:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 window.onload = function(){
     document.body.style.scrollBehavior = "smooth";
 };
-function dropShoe() {
-    alert("Working");
+function dropShoe(){
+
+    const screen = document.getElementById("dropScreen");
+    const shoe = document.getElementById("fallingShoe");
+
+    screen.style.display = "flex";
+
+    shoe.style.animation = "fall 2s forwards";
+
 }
