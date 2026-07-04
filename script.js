6e6e372b7ebe3abead6af98e6bc2f4f04cd61028
clothe-byte <ashahzain123@gmail.com>
2026-07-04 18:36:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,6 @@
 window.onload = function(){
     document.body.style.scrollBehavior = "smooth";
 };
-function dropShoe(){
-
-    const screen =
-        document.getElementById("dropScreen");
-
-    const shoe =
-        document.getElementById("fallingShoe");
-
-    const splash =
-        document.querySelector(".splash");
-
-    // Screen show karo
-    screen.style.display = "flex";
-
-    // Shoe girana
-    shoe.style.animation =
-        "fall 1.5s forwards";
-
-    // Splash chalana
-    setTimeout(function(){
-
-        splash.style.animation =
-            "splashEffect 1s forwards";
-
-    },1200);
-
-    // 3 second baad sab reset
-    setTimeout(function(){
-
-        screen.style.display = "none";
-
-        shoe.style.animation = "";
-
-        splash.style.animation = "";
-
-    },3000);
+function dropShoe() {
+    alert("Working");
 }
