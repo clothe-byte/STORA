a82c843d2e7d5a211278d67d831299651d2bf960
clothe-byte <ashahzain123@gmail.com>
2026-07-04 19:57:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-window.onload = function(){
+window.onload = function () {
     document.body.style.scrollBehavior = "smooth";
 };
-function dropShoe(){
 
-    const screen =
-        document.getElementById("dropScreen");
+function dropShoe() {
 
-    const shoe =
-        document.getElementById("fallingShoe");
+    const screen = document.getElementById("dropScreen");
+    const shoe = document.getElementById("fallingShoe");
+    const splash = document.querySelector(".splash");
 
+    // Screen show
     screen.style.display = "flex";
 
-    shoe.style.animation =
-        shoe.style.top="40%;
+    // Animation reset
+    shoe.style.animation = "none";
+    splash.style.animation = "none";
+
+    // Force refresh
+    shoe.offsetHeight;
+
+    // Start shoe fall
+    shoe.style.animation = "fall 2s forwards";
+
+    // Splash after 1.5 sec
+    setTimeout(() => {
+        splash.style.animation =
+            "splashEffect 0.7s forwards";
+    }, 1500);
+
+    // Close after 4 sec
+    setTimeout(() => {
+        screen.style.display = "none";
+        shoe.style.animation = "none";
+        splash.style.animation = "none";
+    }, 4000);
 }
